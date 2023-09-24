5025c6c0f527bf34b536708bc05f3d7d9ad1286f
samohub <48406077+samohub@users.noreply.github.com>
2023-09-24 09:06:01 +0200

    terms added

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,9 @@
   <div
     class="h-40 bg-sky-900 flex flex-col text-white justify-center items-center"
   >
-    <div>© Coding Delta, as of 2023</div>
+    <div>© Coding Delta B.V., as of 2023</div>
     <a href="/privacy-policy.pdf" target="_blank">Privacy Policy</a>
+    <a href="/terms.pdf">Terms and Conditions</a>
   </div>
 </template>
 
